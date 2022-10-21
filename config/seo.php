0a06d7a679e44d79310c209ea34a7315b6bc4315
Bas van Dinther <bas@vormkracht10.nl>
2022-10-21 12:02:31 +0200

    Seo config

--- config/seo.php
+++ config/seo.php
@@ -1,6 +1,18 @@
 <?php
 
-// config for Vormkracht10/Seo
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Checks
+    |--------------------------------------------------------------------------
+    |
+    | Here you can specify which pages you want to check. It is possible to
+    | specify a model which implements the SeoScore interface. This way you
+    | can check the SEO score of a specific page. 
+    |
+    */
+    'pages' => [
+        'model' => '',
+    ]
 ];
