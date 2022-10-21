10985260ce25a487b673724ce98f9b130a2b3174
Bas van Dinther <bas@vormkracht10.nl>
2022-10-21 14:33:19 +0200

    Add SEO score migration

---
+++ database/migrations/create_seo_score_columns.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $model = config('seo.pages.model');
+
+        Schema::table((new $model)->getTable(), function (Blueprint $table) {
+            $table->integer('seo_score')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+};
--- src/SeoServiceProvider.php
+++ src/SeoServiceProvider.php
@@ -10,16 +10,11 @@ class SeoServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-seo')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_laravel-seo_table')
+            ->hasMigration('create_seo_score_columns')
             ->hasCommand(SeoCommand::class);
     }
 }
