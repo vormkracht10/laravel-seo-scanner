03c8d0a0c80cfeb913613763537f2e3b41163a74
Bas van Dinther <bas@vormkracht10.nl>
2022-10-27 10:40:47 +0200

    Remove a lot of spaces

--- src/Commands/SeoCheck.php
+++ src/Commands/SeoCheck.php
@@ -21,9 +21,7 @@ class SeoCheck extends Command
         $model = config('seo.pages.model');
 
         $model = new $model();
-
-    
-
+        
         $model::all()->filter->url->map(function ($model) {
 
             $seo = $model->seoScore();
