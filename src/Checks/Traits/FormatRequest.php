f0dd99f474a8162aa1b8cc5419d87ecabbb50012
Bas van Dinther <bas@vormkracht10.nl>
2022-11-04 11:45:12 +0100

    Fail all other requests when first one failed

--- src/Checks/Traits/FormatRequest.php
+++ src/Checks/Traits/FormatRequest.php
@@ -6,6 +6,11 @@ trait FormatRequest
 {
     public function formatRequest($request): array
     {
+        // If the first check (ResponseCheck) fails, all other checks should fail.
+        if (! $request['checks'][0]->checkSuccessful) {
+            $this->checkSuccessful = false;
+        }
+
         $previousChecks = $request['checks'];
         $previousChecks[] = $this;
 
