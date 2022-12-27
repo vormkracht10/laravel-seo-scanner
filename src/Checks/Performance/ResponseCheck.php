3733f4864f6427fd2e53d3a3599ec33db3487ca9
Bas van Dinther <bas@vormkracht10.nl>
2022-12-27 11:12:36 +0100

    Remove comments

--- src/Checks/Performance/ResponseCheck.php
+++ src/Checks/Performance/ResponseCheck.php
@@ -7,19 +7,14 @@ use Illuminate\Http\Client\Response;
 
 class ResponseCheck
 {
-    /** @var string */
     public string $title = 'Check if the response is successful';
 
-    /** @var string */
     public string $priority = 'high';
 
-    /** @var int */
     public int $timeToFix = 10;
 
-    /** @var int */
     public int $scoreWeight = 5;
 
-    /** @var bool */
     public bool $checkSuccessful = false;
 
     public function handle(Response $request, Closure $next): array
