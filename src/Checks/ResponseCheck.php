44ebf629a42afc2bf91639f031af143deaa5d664
Bas van Dinther <bas@vormkracht10.nl>
2022-12-27 09:42:50 +0100

    Fix type hinting

--- src/Checks/ResponseCheck.php
+++ src/Checks/ResponseCheck.php
@@ -3,20 +3,26 @@
 namespace Vormkracht10\Seo\Checks;
 
 use Closure;
+use Illuminate\Http\Request;
 
-class ResponseCheck implements Check
+class ResponseCheck
 {
+    /** @var string $title */
     public string $title = 'Check if the response is successful';
 
+    /** @var string $priority */
     public string $priority = 'high';
 
+    /** @var int $timeToFix */
     public int $timeToFix = 10;
 
+    /** @var int $scoreWeight */
     public int $scoreWeight = 5;
 
+    /** @var bool $checkSuccessful */
     public bool $checkSuccessful = false;
 
-    public function handle($request, Closure $next): array
+    public function handle(Request $request, Closure $next): array
     {
         $this->checkSuccessful = false;
 
