8925d9ce11e0fcb1c19a92d54cfc33277a12e10e
Bas van Dinther <bas@vormkracht10.nl>
2022-12-27 11:33:20 +0100

    Add TTFB check

---
+++ src/Checks/Performance/TTFBCheck.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Vormkracht10\Seo\Checks\Performance;
+
+use Closure;
+use Illuminate\Http\Client\Response;
+use Vormkracht10\Seo\Traits\FormatRequest;
+
+class TTFBCheck 
+{
+    use FormatRequest;
+
+    public string $title = "Check if 'Time To First Byte' is below 600 ms";
+
+    public string $priority = 'high';
+
+    public int $timeToFix = 15;
+
+    public int $scoreWeight = 10;
+
+    public bool $checkSuccessful = false;
+
+    public function handle(array $request, Closure $next): array
+    {
+        $ttfb = $request[0]->transferStats->getHandlerStats()['starttransfer_time'] ?? 0;
+
+        if ($ttfb < 0.6) {
+            $this->checkSuccessful = true;
+        }
+
+        return $next($this->formatRequest($request));
+    }
+}
