611bf233f5fc702c12755fad26298b9d2ef44273
Bas van Dinther <bas@vormkracht10.nl>
2023-01-02 10:37:26 +0100

    Rename test and add test to check if all checks can be run

--- tests/SeoTest.php
+++ tests/SeoTest.php
@@ -5,6 +5,32 @@ it('can run the SEO check for a single URL', function () {
         ->assertExitCode(0);
 });
 
+it('can only run configured checks for a single url', function () {
+    config('seo', [
+        'check_routes' => false,
+        'checks' => [
+            \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
+        ],
+        ...config('seo'),
+    ]);
+
+    $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
+        ->expectsOutputToContain('1 out of ' . getCheckCount() . ' checks.')
+        ->assertExitCode(0);
+});
+
+it('can run all checks for a single url', function () {
+    config('seo', [
+        'check_routes' => false,
+        'checks' => [],
+        ...config('seo'),
+    ]);
+
+    $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
+        ->expectsOutputToContain(getCheckCount() . ' out of ' . getCheckCount() . ' checks.')
+        ->assertExitCode(0);
+});
+
 it('can run the SEO check for routes', function () {
     config('seo', [
         'checks' => [
@@ -17,19 +43,5 @@ it('can run the SEO check for routes', function () {
     ]);
 
     $this->artisan('seo:check')
-        ->assertExitCode(0);
-});
-
-it('can only run configured checks', function () {
-    config('seo', [
-        'check_routes' => false,
-        'checks' => [
-            \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
-        ],
-        ...config('seo'),
-    ]);
-
-    $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
-        ->expectsOutputToContain('1 out of ' . getCheckCount() . ' checks.')
         ->assertExitCode(0);
 });
