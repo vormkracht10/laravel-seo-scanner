9c9eb7d1c728b8bee34bba1c877f749ed489a904
Bas van Dinther <bas@vormkracht10.nl>
2023-01-03 14:32:20 +0100

    ContentLengthCheck test

---
+++ tests/Checks/Content/ContentLengthCheckTest.php
@@ -0,0 +1,47 @@
+<?php 
+
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\DomCrawler\Crawler;
+use Vormkracht10\Seo\Checks\Content\ContentLengthCheck;
+
+it('can perform the content length check on content with a length of 2100 characters', function () {
+    $check = new ContentLengthCheck();
+    $crawler = new Crawler();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response( 
+            '<html>
+                <head>
+                    <title>Test</title>
+                </head>
+                <body>
+                    <p>'.str_repeat('a', 2100).'</p>
+                </body>',
+        200),
+    ]);
+
+    $crawler->addHtmlContent(Http::get('vormkracht10.nl')->body());
+
+    $this->assertTrue($check->check(Http::get('vormkracht10.nl'), $crawler));
+});
+
+it('can perform the content length check on content with less characters', function () {
+    $check = new ContentLengthCheck();
+    $crawler = new Crawler();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response( 
+            '<html>
+                <head>
+                    <title>Test</title>
+                </head>
+                <body>
+                    <p>'.str_repeat('a', 100).'</p>
+                </body>',
+        200),
+    ]);
+
+    $crawler->addHtmlContent(Http::get('vormkracht10.nl')->body());
+
+    $this->assertFalse($check->check(Http::get('vormkracht10.nl'), $crawler));
+});
