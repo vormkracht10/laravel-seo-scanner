e3bf51fd8e7f427ab124db38ebb5b5915b8510db
Bas van Dinther <bas@vormkracht10.nl>
2023-01-03 11:32:47 +0100

    Alt tag check tests and check if alt is empty

---
+++ tests/ContentCheckTest.php
@@ -0,0 +1,45 @@
+<?php 
+
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\DomCrawler\Crawler;
+use Vormkracht10\Seo\Checks\Content\AltTagCheck;
+
+it('can perform the alt tag check with alt', function () {
+    $check = new AltTagCheck();
+    $crawler = new Crawler();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response('<html><head></head><body><img src="https://vormkracht10.nl/images/logo.png" alt="Vormkracht10 logo"></body></html>', 200),
+    ]);
+
+    $crawler->addHtmlContent(Http::get('vormkracht10.nl')->body());
+
+    $this->assertTrue($check->check(Http::get('vormkracht10.nl'), $crawler));
+});
+
+it('can perform the alt tag check without alt', function () {
+    $check = new AltTagCheck();
+    $crawler = new Crawler();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response('<html><head></head><body><img src="https://vormkracht10.nl/images/logo.png"></body></html>', 200),
+    ]);
+
+    $crawler->addHtmlContent(Http::get('vormkracht10.nl')->body());
+
+    $this->assertFalse($check->check(Http::get('vormkracht10.nl'), $crawler));
+});
+
+it('can perform the alt tag check with empty alt', function () {
+    $check = new AltTagCheck();
+    $crawler = new Crawler();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response('<html><head></head><body><img src="https://vormkracht10.nl/images/logo.png" alt=""></body></html>', 200),
+    ]);
+
+    $crawler->addHtmlContent(Http::get('vormkracht10.nl')->body());
+
+    $this->assertFalse($check->check(Http::get('vormkracht10.nl'), $crawler));
+});
+
