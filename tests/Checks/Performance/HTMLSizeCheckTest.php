1811649e36d206321050d9e5376db86304a2ea2b
Bas van Dinther <bas@vormkracht10.nl>
2023-01-03 13:26:07 +0100

    HTML and ImageSize check

---
+++ tests/Checks/Performance/HTMLSizeCheckTest.php
@@ -0,0 +1,27 @@
+<?php 
+
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\DomCrawler\Crawler;
+use Vormkracht10\Seo\Checks\Performance\HTMLSizeCheck;
+
+it('can perform the HTML size check on HTML that is smaller than 100 KB', function () {
+    $check = new HTMLSizeCheck();
+    $crawler = new Crawler();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response('<html><head></head><body>sljfalsfdjka</body></html>', 200),
+    ]);
+
+    $this->assertFalse($check->check(Http::get('vormkracht10.nl'), $crawler));
+});
+
+it('can perform the HTML size check on HTML that is larger than 100 KB', function () {
+    $check = new HTMLSizeCheck();
+    $crawler = new Crawler();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response('<html><head></head><body>'.str_repeat('sljfalsfdjka', 10000).'</body></html>', 200),
+    ]);
+
+    $this->assertTrue($check->check(Http::get('vormkracht10.nl'), $crawler));
+});
---
+++ tests/Checks/Performance/ImageSizeCheckTest.php
@@ -0,0 +1,45 @@
+<?php 
+
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\DomCrawler\Crawler;
+use Vormkracht10\Seo\Checks\Performance\ImageSizeCheck;
+
+it('can perform the image size check on broken images', function () {
+    $check = new ImageSizeCheck();
+    $crawler = new Crawler();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response('<html><head></head><body><img src="https://vormkracht10.nl/404"></body></html>', 200),
+    ]);
+
+    $crawler->addHtmlContent(Http::get('vormkracht10.nl')->body());
+
+    $this->assertFalse($check->check(Http::get('vormkracht10.nl'), $crawler));
+});
+
+it('can perform the image size check on small images', function () {
+    $check = new ImageSizeCheck();
+    $crawler = new Crawler();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response('<html><head></head><body><img srct="https://source.unsplash.com/random/100x100"></body></html>', 200),
+    ]);
+
+    $crawler->addHtmlContent(Http::get('vormkracht10.nl')->body());
+
+    $this->assertTrue($check->check(Http::get('vormkracht10.nl'), $crawler));
+});
+
+it('can perform the image size check on large images', function () {
+    $check = new ImageSizeCheck();
+    $crawler = new Crawler();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response('<html><head></head><body><img src="https://source.unsplash.com/random/5000x5000"></body></html>', 200),
+    ]);
+
+    $crawler->addHtmlContent(Http::get('vormkracht10.nl')->body());
+
+    $this->assertFalse($check->check(Http::get('vormkracht10.nl'), $crawler));
+});
+
