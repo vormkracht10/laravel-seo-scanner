30625a15405745c57bf349e11e4dfae3e1a6e46d
Bas van Dinther <bas@vormkracht10.nl>
2023-01-03 13:07:21 +0100

    Add ResponseCheck and CompressionCheck tests

---
+++ tests/Checks/Performance/CompressionCheckTest.php
@@ -0,0 +1,31 @@
+<?php 
+
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\DomCrawler\Crawler;
+use Vormkracht10\Seo\Checks\Performance\CompressionCheck;
+
+it('can perform a compression check on a compressed response', function () {
+    $check = new CompressionCheck();
+
+    $contentEncodings = ['gzip', 'deflate', 'br', 'compress'];
+
+    foreach ($contentEncodings as $contentEncoding) {
+        Http::fake([
+            'vormkracht10.nl' => Http::response('<html><head></head><body></body></html>', 200, [
+                'Content-Encoding' => $contentEncoding,
+            ]),
+        ]);
+
+        $this->assertTrue($check->check(Http::get('vormkracht10.nl'), new Crawler()));
+    }
+});
+
+it('can perform a compression check on a non-compressed response', function () {
+    $check = new CompressionCheck();
+
+    Http::fake([
+        'vormkracht10.nl' => Http::response('<html><head></head><body></body></html>', 200),
+    ]);
+
+    $this->assertFalse($check->check(Http::get('vormkracht10.nl'), new Crawler()));
+});
