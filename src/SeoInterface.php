5a0a646aa15b3fc3f0b978328db1367b91611c9e
Bas van Dinther <bas@vormkracht10.nl>
2023-01-04 13:14:45 +0100

    Dont enforce to implement other methods than the url attribute as theyre not needed

--- src/SeoInterface.php
+++ src/SeoInterface.php
@@ -8,15 +8,5 @@ use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 interface SeoInterface
 {
-    public function seoScore(): SeoScore;
-
-    public function seoScores(): MorphMany;
-
-    public function scopeWithSeoScores(Builder $query): Builder;
-
-    public function getCurrentScore(): int;
-
     public function getUrlAttribute(): string|null;
-
-    public function getCurrentScoreDetails(): array;
 }
