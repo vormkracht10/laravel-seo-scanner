580497faeae1be652ae1954694fc6d622153d43d
Bas van Dinther <bas@vormkracht10.nl>
2023-01-04 11:27:26 +0100

    Add explanation and type hinting

--- tests/Checks/Performance/TTFBCheckTest.php
+++ tests/Checks/Performance/TTFBCheckTest.php
@@ -11,9 +11,19 @@ it('can perform the ttfb check', function () {
         'vormkracht10.nl/robots.txt' => Http::response('<html></html>', 200),
     ]);
 
-    if ($check->check(Http::get('vormkracht10.nl'), new Crawler()) && $check->actualValue > $check->expectedValue) {
-        $this->assertFalse($check->check(Http::get('vormkracht10.nl'), new Crawler()));
+    /** @var Illuminate\Http\Client\Response $response */
+    $response = Http::get('vormkracht10.nl');
+    
+    /**
+     * Because we can't manually set the ttfb value, we'll just check if the check
+     * returns true or false. If it returns false, we'll check if the actual value
+     * is higher than the expected value. If it is, we'll check if the check returns
+     * false. If it doesn't, we'll check if the check returns true.
+     * 
+     */
+    if ($check->check($response, new Crawler()) && $check->actualValue > $check->expectedValue) {
+        $this->assertFalse($check->check($response, new Crawler()));
     } else {
-        $this->assertTrue($check->check(Http::get('vormkracht10.nl'), new Crawler()));
+        $this->assertTrue($check->check($response, new Crawler()));
     }
 });
