09ba379d49297c6587c2caa419b32116de947655
Bas van Dinther <bas@vormkracht10.nl>
2023-01-04 11:20:52 +0100

    Test if we can read the actual values from the check

--- tests/Checks/Performance/TTFBCheckTest.php
+++ tests/Checks/Performance/TTFBCheckTest.php
@@ -4,12 +4,16 @@ use Illuminate\Support\Facades\Http;
 use Symfony\Component\DomCrawler\Crawler;
 use Vormkracht10\Seo\Checks\Performance\TTFBCheck;
 
-it('can perform the ttfb check and successfully get the url from handler stats', function () {
+it('can perform the ttfb check', function () {
     $check = new TTFBCheck();
 
     Http::fake([
         'vormkracht10.nl/robots.txt' => Http::response('<html></html>', 200),
     ]);
 
-    $this->assertTrue($check->check(Http::get('vormkracht10.nl'), new Crawler()));
+    if ($check->check(Http::get('vormkracht10.nl'), new Crawler()) && $check->actualValue > $check->expectedValue) {
+        $this->assertFalse($check->check(Http::get('vormkracht10.nl'), new Crawler()));
+    } else {
+        $this->assertTrue($check->check(Http::get('vormkracht10.nl'), new Crawler()));
+    }
 });
