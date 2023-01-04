8ebc7a369d8c263b159c47372c32a4ebfaa02881
Bas van Dinther <bas@vormkracht10.nl>
2023-01-04 11:36:50 +0100

    Uncomment tests

--- tests/SeoTest.php
+++ tests/SeoTest.php
@@ -1,61 +1,61 @@
 <?php
 
-// it('can run the SEO check for a single URL', function () {
-//     $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
-//         ->assertExitCode(0);
-// });
+it('can run the SEO check for a single URL', function () {
+    $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
+        ->assertExitCode(0);
+});
 
-// it('can only run configured checks for a single url', function () {
-//     config('seo', [
-//         'check_routes' => false,
-//         'checks' => [
-//             \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
-//         ],
-//         ...config('seo'),
-//     ]);
+it('can only run configured checks for a single url', function () {
+    config('seo', [
+        'check_routes' => false,
+        'checks' => [
+            \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
+        ],
+        ...config('seo'),
+    ]);
 
-//     $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
-//         ->expectsOutputToContain('1 out of '.getCheckCount().' checks.')
-//         ->assertExitCode(0);
-// });
+    $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
+        ->expectsOutputToContain('1 out of '.getCheckCount().' checks.')
+        ->assertExitCode(0);
+});
 
-// it('can run all checks for a single url', function () {
-//     config('seo', [
-//         'check_routes' => false,
-//         'checks' => [],
-//         ...config('seo'),
-//     ]);
+it('can run all checks for a single url', function () {
+    config('seo', [
+        'check_routes' => false,
+        'checks' => [],
+        ...config('seo'),
+    ]);
 
-//     $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
-//         ->expectsOutputToContain(getCheckCount().' out of '.getCheckCount().' checks.')
-//         ->assertExitCode(0);
-// });
+    $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
+        ->expectsOutputToContain(getCheckCount().' out of '.getCheckCount().' checks.')
+        ->assertExitCode(0);
+});
 
-// it('can run the SEO check for routes', function () {
-//     config('seo', [
-//         'checks' => [
-//             \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
-//         ],
-//         'routes' => [
-//             'https://vormkracht10.nl',
-//         ],
-//         ...config('seo'),
-//     ]);
+it('can run the SEO check for routes', function () {
+    config('seo', [
+        'checks' => [
+            \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
+        ],
+        'routes' => [
+            'https://vormkracht10.nl',
+        ],
+        ...config('seo'),
+    ]);
 
-//     $this->artisan('seo:check')
-//         ->assertExitCode(0);
-// });
+    $this->artisan('seo:check')
+        ->assertExitCode(0);
+});
 
-// it('can only run configured checks', function () {
-//     config('seo', [
-//         'check_routes' => false,
-//         'checks' => [
-//             \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
-//         ],
-//         ...config('seo'),
-//     ]);
+it('can only run configured checks', function () {
+    config('seo', [
+        'check_routes' => false,
+        'checks' => [
+            \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
+        ],
+        ...config('seo'),
+    ]);
 
-//     $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
-//         ->expectsOutputToContain('1 out of '.getCheckCount().' checks.')
-//         ->assertExitCode(0);
-// });
+    $this->artisan('seo:check-url', ['url' => 'https://vormkracht10.nl'])
+        ->expectsOutputToContain('1 out of '.getCheckCount().' checks.')
+        ->assertExitCode(0);
+});
