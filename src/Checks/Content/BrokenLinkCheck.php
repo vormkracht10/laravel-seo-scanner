9c3e0990ffe23da4b6aa838aea56a5a1016d15ff
Bas van Dinther <bas@vormkracht10.nl>
2023-01-06 16:35:54 +0100

    Remove dev code

--- src/Checks/Content/BrokenLinkCheck.php
+++ src/Checks/Content/BrokenLinkCheck.php
@@ -29,23 +29,11 @@ class BrokenLinkCheck implements Check
 
     public function check(Response $response, Crawler $crawler): bool
     {
-        // if (! $this->validateContent($crawler)) {
-        //     return false;
-        // }
+        if (! $this->validateContent($crawler)) {
+            return false;
+        }
 
-        $this->actualValue = [
-            // Array with links
-            'https://www.google.com',
-            'https://facebook.com',
-            'https://twitter.com',
-            'https://www.youtube.com',
-            'https://www.instagram.com',
-            'https://www.linkedin.com',
-            'https://www.pinterest.com',
-            'https://www.tiktok.com',
-        ];
-
-        return false;
+        return true;
     }
 
     public function validateContent(Crawler $crawler): bool
