74a67d940feb0673d6d1548041e64c4969798b6a
Bas van Dinther <bas@vormkracht10.nl>
2023-01-06 21:00:34 +0100

    Clean up tests

--- tests/SeoTest.php
+++ tests/SeoTest.php
@@ -5,58 +5,28 @@ it('can run the SEO check for a single URL', function () {
         ->assertExitCode(0);
 });
 
-it('can only run configured checks for a single url', function () {
-    config('seo', [
-        'check_routes' => false,
-        'checks' => [
-            \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
-        ],
-    ]);
-
-    $this->artisan('seo:scan-url', ['url' => 'https://vormkracht10.nl'])
-        ->expectsOutputToContain('1 out of '.getCheckCount().' checks.')
-        ->assertExitCode(0);
-});
-
-it('can run all checks for a single url', function () {
-    config('seo', [
-        'check_routes' => false,
-        'database' => [
-            'save' => false,
-        ],
-        'checks' => [],
-    ]);
-
-    $this->artisan('seo:scan-url', ['url' => 'https://vormkracht10.nl'])
-        ->expectsOutputToContain(getCheckCount().' out of '.getCheckCount().' checks.')
-        ->assertExitCode(0);
-});
-
 it('can run the SEO check for routes', function () {
-    config('seo', [
-        'checks' => [
-            \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
-        ],
-        'routes' => [
-            'https://vormkracht10.nl',
-        ],
-    ]);
-
     config(['seo.database.save' => false]);
+    config(['seo.routes' => [
+        'https://vormkracht10.nl',
+    ]]);
+    config(['seo.checks' => [
+        \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
+    ]]);
+
 
     $this->artisan('seo:scan')
         ->assertExitCode(0);
 });
 
 it('can only run configured checks', function () {
-    config('seo', [
-        'check_routes' => false,
-        'checks' => [
+    config(['seo.database.save' => false]);
+    config(['seo.check_routes' => false]);
+    config(['seo.checks' =>
+        [
             \Vormkracht10\Seo\Checks\Content\MultipleHeadingCheck::class,
         ],
     ]);
-
-    config(['seo.database.save' => false]);
 
     $this->artisan('seo:scan-url', ['url' => 'https://vormkracht10.nl'])
         ->expectsOutputToContain('1 out of '.getCheckCount().' checks.')
