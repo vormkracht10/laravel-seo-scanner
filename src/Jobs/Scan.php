073ec660abb68ff5a0fc014f9e0559220954a5b9
Bas van Dinther <bas@vormkracht10.nl>
2023-04-12 10:16:59 +0200

    Use native Laravel job instead of Laravel Actions

---
+++ src/Jobs/Scan.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Vormkracht10\Seo\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
+
+class Scan implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $tries = 1;
+    public $timeout = 60 * 60 * 3;
+    
+    public function __construct()
+    {
+        $this->onQueue('seo');
+    }
+
+    public function handle(): void
+    {
+        Artisan::call('seo:scan');
+    }
+}
