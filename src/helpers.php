3e15dada2f2a151c67b94f1eb4539ed219fdcc39
Bas van Dinther <bas@vormkracht10.nl>
2023-05-26 15:15:52 +0200

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -59,7 +59,7 @@ if (! function_exists('getRemoteStatus')) {
 
             curl_setopt_array($handle, $options);
             $data = curl_exec($handle);
-            dd($data);
+            dd($data, $options);
 
             $statusCode = curl_getinfo($handle, CURLINFO_RESPONSE_CODE);
 
