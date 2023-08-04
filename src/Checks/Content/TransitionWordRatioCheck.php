9b08aa46f1a1847c69f181ee417db1a23a9000b5
Baspa <Baspa@users.noreply.github.com>
2023-08-04 14:04:22 +0000

    Fix styling

--- src/Checks/Content/TransitionWordRatioCheck.php
+++ src/Checks/Content/TransitionWordRatioCheck.php
@@ -2,12 +2,11 @@
 
 namespace Vormkracht10\Seo\Checks\Content;
 
-use Illuminate\Support\Str;
 use Illuminate\Http\Client\Response;
-use Vormkracht10\Seo\Interfaces\Check;
 use Symfony\Component\DomCrawler\Crawler;
-use Vormkracht10\Seo\Traits\PerformCheck;
 use Vormkracht10\Seo\Checks\Helpers\TransitionWords;
+use Vormkracht10\Seo\Interfaces\Check;
+use Vormkracht10\Seo\Traits\PerformCheck;
 
 class TransitionWordRatioCheck implements Check
 {
--- src/Checks/Helpers/TransitionWords.php
+++ src/Checks/Helpers/TransitionWords.php
@@ -1,52 +1,52 @@
-<?php 
+<?php
 
 namespace Vormkracht10\Seo\Checks\Helpers;
 
 class TransitionWords
 {
     public static array $transitionWords = [
-        "addition" => [
-            "additionally", "moreover", "furthermore", "in addition",
-            "not only", "but also", "as well as", "besides", "what's more"
+        'addition' => [
+            'additionally', 'moreover', 'furthermore', 'in addition',
+            'not only', 'but also', 'as well as', 'besides', "what's more",
         ],
-        "contrast" => [
-            "however", "nevertheless", "on the other hand", "in contrast",
-            "conversely", "although", "while", "yet", "even though", "nonetheless"
+        'contrast' => [
+            'however', 'nevertheless', 'on the other hand', 'in contrast',
+            'conversely', 'although', 'while', 'yet', 'even though', 'nonetheless',
         ],
-        "comparison" => [
-            "similarly", "likewise", "in comparison", "just as",
-            "compared to", "similarly to"
+        'comparison' => [
+            'similarly', 'likewise', 'in comparison', 'just as',
+            'compared to', 'similarly to',
         ],
-        "cause_and_effect" => [
-            "therefore", "thus", "consequently", "as a result", "because",
-            "since", "so", "due to", "owing to", "accordingly"
+        'cause_and_effect' => [
+            'therefore', 'thus', 'consequently', 'as a result', 'because',
+            'since', 'so', 'due to', 'owing to', 'accordingly',
         ],
-        "emphasis" => [
-            "indeed", "certainly", "of course", "undoubtedly",
-            "without a doubt", "naturally"
+        'emphasis' => [
+            'indeed', 'certainly', 'of course', 'undoubtedly',
+            'without a doubt', 'naturally',
         ],
-        "example" => [
-            "for example", "for instance", "such as", "to illustrate", "in particular"
+        'example' => [
+            'for example', 'for instance', 'such as', 'to illustrate', 'in particular',
         ],
-        "sequence_order" => [
-            "first, second, third", "next", "then", "afterward", "meanwhile",
-            "finally", "in the meantime", "subsequently"
+        'sequence_order' => [
+            'first, second, third', 'next', 'then', 'afterward', 'meanwhile',
+            'finally', 'in the meantime', 'subsequently',
         ],
-        "conclusion_summary" => [
-            "in conclusion", "to sum up", "ultimately", "in summary", "all in all", "overall"
+        'conclusion_summary' => [
+            'in conclusion', 'to sum up', 'ultimately', 'in summary', 'all in all', 'overall',
         ],
-        "time" => [
-            "meanwhile", "before", "after", "during", "while", "since", "until", "eventually", "soon", "in the past", "in the future"
+        'time' => [
+            'meanwhile', 'before', 'after', 'during', 'while', 'since', 'until', 'eventually', 'soon', 'in the past', 'in the future',
         ],
-        "clarification" => [
-            "in other words", "that is to say", "specifically", "to clarify"
+        'clarification' => [
+            'in other words', 'that is to say', 'specifically', 'to clarify',
         ],
-        "illustration" => [
-            "specifically", "in this case", "an example of this is", "to demonstrate"
+        'illustration' => [
+            'specifically', 'in this case', 'an example of this is', 'to demonstrate',
         ],
-        "concession" => [
-            "admittedly", "granted", "even though", "while it is true"
-        ]
+        'concession' => [
+            'admittedly', 'granted', 'even though', 'while it is true',
+        ],
     ];
 
     public static function getTransitionWords(): array
