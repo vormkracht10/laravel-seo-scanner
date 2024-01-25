52f98f8c3f679d738b19ae645c2ed0478c6dfad7
Baspa <Baspa@users.noreply.github.com>
2024-01-25 10:10:45 +0000

    Fix styling

--- src/Checks/Meta/DescriptionCheck.php
+++ src/Checks/Meta/DescriptionCheck.php
@@ -41,28 +41,28 @@ class DescriptionCheck implements Check
     public function getDescriptionContent(Crawler $crawler): ?string
     {
         $tags = ['description', 'og:description', 'twitter:description'];
-    
+
         foreach ($tags as $tag) {
             $property = $tag === 'og:description' ? 'property' : 'name';
-            
+
             /** @var \DOMElement $node */
             $node = $crawler->filterXPath("//meta[@{$property}=\"{$tag}\"]")->getNode(0);
-    
+
             if ($node) {
                 return $node->getAttribute('content');
             }
         }
-    
+
         return null;
     }
-    
+
     public function validateContent(Crawler $crawler): bool
     {
         $content = $this->getDescriptionContent($crawler);
-    
-        return !empty($content);
+
+        return ! empty($content);
     }
-    
+
     public function isDescriptionSet(Crawler $crawler): bool
     {
         return $this->getDescriptionContent($crawler) !== null;
